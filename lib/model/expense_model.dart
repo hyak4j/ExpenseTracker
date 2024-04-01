6628ebe5450583002c832a8f8c453304f52fffd7
hyak4j <65548056+hyak4j@users.noreply.github.com>
2024-04-01 22:44:55 +0800

    feat: 記帳列表初步建置及測試

---
+++ lib/model/expense_model.dart
@@ -0,0 +1,21 @@
+
+import 'package:uuid/uuid.dart';
+
+const uuid = Uuid();
+
+enum ExpenseCategory { food, travel, leisure, work }
+
+class ExpenseModel {
+  ExpenseModel({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final ExpenseCategory category;
+}
---
+++ lib/view/expenses_screen.dart
@@ -0,0 +1,42 @@
+import 'package:expense_tracker/view/expenses_list.dart';
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/model/expense_model.dart';
+import 'package:flutter/widgets.dart';
+
+class ExpensesScreen extends StatefulWidget {
+  const ExpensesScreen({super.key});
+
+  @override
+  State<ExpensesScreen> createState() => _ExpensesScreenState();
+}
+
+class _ExpensesScreenState extends State<ExpensesScreen> {
+  final List<ExpenseModel> _registeredExpenses = [
+    ExpenseModel(
+      title: 'Fit Course', 
+      amount: 18.02, 
+      date: DateTime.now(),
+      category: ExpenseCategory.work,  
+    ),
+    ExpenseModel(
+      title: 'Pork',
+      amount: 0.95,
+      date: DateTime.now(),
+      category: ExpenseCategory.food,
+    ),
+  ];
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('The Chart'),
+          Expanded(
+            child: ExpensesList(expenses: _registeredExpenses)
+          ),
+        ],
+      ),
+    );
+  }
+}
