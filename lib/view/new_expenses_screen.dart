711e36f8be5904c421453eb0361f684341e19b6b
hyak4j <65548056+hyak4j@users.noreply.github.com>
2024-04-10 22:12:47 +0800

    feat: 日期選取功能

--- lib/view/new_expenses_screen.dart
+++ lib/view/new_expenses_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:expense_tracker/model/expense_model.dart';
 
 class NewExpenseScreen extends StatefulWidget {
   const NewExpenseScreen({super.key});
@@ -10,6 +12,21 @@ class NewExpenseScreen extends StatefulWidget {
 class _NewExpenseScreenState extends State<NewExpenseScreen> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
+  DateTime? _selectedDate;
+
+  void _presentDatePicker() async{
+    // 範圍一年前至今
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1, now.month, now.day);
+    final pickedDate = await showDatePicker(
+      context: context,
+      initialDate: now, 
+      firstDate: firstDate, 
+      lastDate: now);
+    setState(() {
+      _selectedDate = pickedDate;
+    });
+  }
 
   @override
   void dispose() {
@@ -31,13 +48,34 @@ class _NewExpenseScreenState extends State<NewExpenseScreen> {
               label: Text('Title')
             ),
           ),
-          TextField(
-            keyboardType: TextInputType.number,
-            controller: _amountController,
-            decoration: const InputDecoration(
-              prefixText: '\$ ',
-              label: Text('Amount')
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                keyboardType: TextInputType.number,
+                controller: _amountController,
+                decoration: const InputDecoration(
+                  prefixText: '\$ ',
+                  label: Text('Amount')
+                ),
+                          ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(_selectedDate == null? 'No date selected' : formatter.format(_selectedDate!)),
+                    IconButton(
+                      onPressed: _presentDatePicker, 
+                      icon: const Icon(
+                        Icons.calendar_month
+                        ),
+                      )
+                  ],
+                ),
+              )
+            ],
           ),
           Row(
             children: [
