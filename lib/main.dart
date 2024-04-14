fbd7b0d3e7a21e35a947bd5409e3a9fc782edd73
hyak4j <65548056+hyak4j@users.noreply.github.com>
2024-04-14 22:43:20 +0800

    feat: 主題配色支援深色模式 (Dark Mode)

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,28 @@ import 'package:expense_tracker/view/expenses_screen.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
-  seedColor: Color.fromARGB(255, 196, 160, 82));
+  seedColor: const Color.fromARGB(255, 196, 160, 82));
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125)
+);
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer
+          )
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,  // 設定後依照指定顏色會有同系列不同的配色
         appBarTheme: const AppBarTheme().copyWith(
@@ -29,6 +47,7 @@ void main() {
           )
         )
       ),
+      themeMode: ThemeMode.system,
       home: ExpensesScreen(),
     )
   );
