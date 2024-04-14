f1448ebc8f2ec6406d0aad4d1508b099199802a8
hyak4j <65548056+hyak4j@users.noreply.github.com>
2024-04-14 22:19:24 +0800

    feat: 各類元件主題調整

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,34 @@
 import 'package:expense_tracker/view/expenses_screen.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 196, 160, 82));
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,  // 設定後依照指定顏色會有同系列不同的配色
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer
+          )
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+            fontSize: 14
+          )
+        )
+      ),
       home: ExpensesScreen(),
     )
   );
